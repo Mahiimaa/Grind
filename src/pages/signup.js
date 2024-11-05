456c10481162c7bb8c75476943bfa60df827a032
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-11-05 20:45:34 +0545

    pages

---
+++ src/pages/signup.js
@@ -0,0 +1,23 @@
+import  React from 'react';
+
+const Signup = () =>{
+    return(
+        <div>
+            <h2>Sign Up Form</h2>
+            <form>
+                <label>
+                    Username:
+                    <input type="text" name="username" />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" name="password" />
+                </label>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+export default Signup;
