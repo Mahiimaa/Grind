3ecb4ad6092d1a45bb47fbfce4b7e57e58847ffe
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-11-10 23:09:29 +0545

    added nav

---
+++ src/pages/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Nav =() =>{
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+                <li>
+                    <a href="/signup">Sign Up</a>
+                </li>
+                <li>
+                    <a href="/forgot">Forgot Password</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
