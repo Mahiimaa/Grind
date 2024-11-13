c5ecc84b4b4eaa25d98d7e5a4e8dd9d18f0705e1
Mahiimaa <137261863+Mahiimaa@users.noreply.github.com>
2024-11-13 22:16:45 +0545

    l o g i n

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 const Login = () =>{
     return(
-        <form>
-            <label>
+        <div className= 'bg-red-200 w-full h-[100vh] flex justify-center items-center '>
+        <form className='flex flex-col gap-4'>
+            <div className='font-extrabold text-4xl text-blue-700 font-serif'> LOG IN </div>
+            <div className=''>
+            <label className=' font-sans font-semibold text-xl'>
                 Username:
-                <input type="text" name="username" />
+                <input className="bg-slate-400 p-2 rounded-md w-64" type="text" name="username" />
             </label>
-            <label>
-                Password:
-                <input type="password" name="password" />
+            </div> 
+            <label className=' font-sans font-semibold text-xl'>
+                Password: 
+                <input className="bg-slate-400 p-2 rounded-md w-64" type="password" name="password" />
             </label>
             <input type="submit" value="Submit" />
             <p>Don't have an account? Sign Up</p>
             <p>Forgot Password?</p>
         </form>
+        </div>
     )
 }
 export default Login;
